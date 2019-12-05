11cca5f3db41703e82aee2154b9aa402a117aef3
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2019-12-05 15:17:10 +0100

    Added CSV database.

--- src/concessionario/core/Dipendente.java
+++ src/concessionario/core/Dipendente.java
@@ -1,10 +1,16 @@
 package concessionario.core;
 
-public class Dipendente {
+import concessionario.database.csv.CSVSerializerInterface;
+
+public class Dipendente implements CSVSerializerInterface {
 
 	private Integer cid;
 	private String nome;
 	private String cognome;
+	
+	public Dipendente() {
+		
+	}
 	
 	public Dipendente(Integer cid, String nome, String cognome) {
 		this.cid = cid;
@@ -26,6 +32,16 @@ public class Dipendente {
 
 	public String toString() {
 		return this.cid + ". " + this.nome + " " + this.cognome;
+	}
+
+	@Override
+	public String toCSV() {
+		return "dipendente;" + this.cid + ";" + this.nome + ";" + this.cognome;
+	}
+
+	public static Object fromCSV(String s) {
+		String[] values = s.split(";");
+		return new Dipendente(Integer.parseInt(values[1]), values[2], values[3]);
 	}
 	
 }
